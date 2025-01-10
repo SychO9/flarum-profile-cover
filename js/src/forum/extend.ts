8a5aae44ba03875b8d9e7fe135c7ec71c3eb55ec
Sami Mazouz <sychocouldy@gmail.com>
2025-01-10 14:51:39 +0100

    chore: yarn format

--- js/src/forum/extend.ts
+++ js/src/forum/extend.ts
@@ -1,11 +1,6 @@
 import Extend from 'flarum/common/extenders';
-import User from "flarum/common/models/User";
+import User from 'flarum/common/models/User';
 
 export default [
-
-  new Extend.Model(User)
-    .attribute<string|null>('cover')
-    .attribute<string|null>('cover_thumbnail')
-    .attribute<boolean>('canSetProfileCover'),
-
+  new Extend.Model(User).attribute<string | null>('cover').attribute<string | null>('cover_thumbnail').attribute<boolean>('canSetProfileCover'),
 ];
